01692f1353df65bb512e9fc74896caac553bfca3
Oscar <alu0101038230@ull.edu.es>
2021-12-01 18:00:24 +0000

    Ampliada clase zona

--- src/zona.ts
+++ src/zona.ts
@@ -4,12 +4,42 @@ import { PuntoSubmarinismo } from "./puntoSub";
  */
  export class Zona {
   private zoneName: string;
-  private description: string;
+  private zoneDescription: string;
   private pointsArray: PuntoSubmarinismo[] = [];
-  constructor(private nombreZona: string, private descripcion: string,
-    private arrayDePuntos: PuntoSubmarinismo[]) {
+  constructor(private nombreZona: string, private descripcion: string) {
     this.zoneName = nombreZona;
-    this.description = descripcion;
-    this.pointsArray = arrayDePuntos;
+    this.zoneDescription = descripcion;
+  }
+
+  getZoneName() {
+    return this.zoneName;
+  }
+
+  getZoneDescription() {
+    return this.zoneDescription;
+  }
+
+  /**
+   * En la página web se crearía un formulario para introducir los datos del nuevo
+   * punto de submarinismo. Todos esos datos deberían formatearse en un objeto PuntoSubmarinismo
+   * y ese objeto es el que se introduce en este método.
+   */
+  addPointToArray(newPoint: PuntoSubmarinismo) {
+    this.pointsArray.push(newPoint);
+  }
+
+  getPoint(position: number) {
+    return this.pointsArray[position];
+  }
+
+  /**
+   * TODO: Complete finder of Points (Óscar Pozo)
+   */
+  finderOfPoints(matchString: string) {
+    this.pointsArray.forEach(element => {
+      if (element.getPointName() === matchString) {
+
+      }
+    });
   }
 }
