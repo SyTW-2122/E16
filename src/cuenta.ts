6520178c3b5c5f9ae0ce4935115ab19b5f7a1fcf
Oscar <alu0101038230@ull.edu.es>
2021-12-01 17:11:49 +0000

    Creado la clase Encrypt y actualizadas las clases Perfil y Cuenta

--- src/cuenta.ts
+++ src/cuenta.ts
@@ -1,20 +1,40 @@
 import { Perfil } from "./perfil"
+
 /**
- * 
+ * Verificador de formato de un email
+ * @param email Email a validar
+ * @return  Retorna...
+ * ```
+ * comentario
+ * ```
+ */
+ const validateEmail = (email) => {
+  return String(email)
+  .toLowerCase()
+  .match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
+/**
+ * Clase Cuenta que contiene los datos personales del 
  */
 export class Cuenta {
   private UserProfile: Perfil;
   private name: string;
   private surname: string;
-  private age: number;
+  private email: string; /** Se puede crear un struct para correo */
+  private password: string;
   private address: string;
   
-  constructor(perfilUsuario: Perfil, nombre: string, apellidos: string,
-    edad: number, dirección: string) {
+  constructor(private perfilUsuario: Perfil, private nombre: string, private apellidos: string,
+    private correo: string, private contraseña: string, 
+    private dirección: string) {
     this.UserProfile = perfilUsuario;
     this.name = nombre;
     this.surname = apellidos;
-    this.age = edad;
+    this.email = correo;
+    this.password = contraseña;
     this.address = dirección;
   }
 
@@ -23,7 +43,7 @@ export class Cuenta {
   }
   
   getEmail() {
-    return this.UserProfile.getEmail();
+    return this.email;
   }
 
   getLicense() {
@@ -34,7 +54,11 @@ export class Cuenta {
     return this.UserProfile.getAboutMe();
   }
 
-  passwordValidation(password: string) {
-    return this.UserProfile.validatePassword(password);
+  passwordValidation(contraseña: string) {
+    if (contraseña == this.password) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- src/perfil.ts
+++ src/perfil.ts
@@ -3,64 +3,32 @@
 */
 type typeOfLicence = 'A' | 'B' | 'C';
 
-/**
- * Verificador de formato de un email
- * @param email
- * @return
- * ```
- * comentario
- * ```
- */
-const validateEmail = (email) => {
-  return String(email)
-  .toLowerCase()
-  .match(
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  );
-};
 
 export class Perfil {
   /**
    * Constructor de la clase Perfil. Contiene todos los datos de la cuenta de usuario.
    * @param username Nombre de usuario
-   * @param email Correo electrónico de usuario
-   * @param password Contraseña (encriptada) del usuario
    * @param license Tipo de licencia del usuario
+   * @param aboutMe Descripción personalizada del perfil de usuario
    */
   private username: string = '';
-  private email: string = ''; /** Se puede crear un struct para correo */
-  private password: string = '';
+  private age: number;
   private license: typeOfLicence; /** Crear una estructura para las licencias */
   private aboutMe: string = '';
-  constructor(private nombreUsuario: string, private correo: string, 
-    private contraseña: string, private licencia: typeOfLicence, 
-    private descripción: string) {
-      if (validateEmail(correo)) {
-        this.username = nombreUsuario;
-        this.email = correo;
-        this.password = contraseña;
-        this.license = licencia;
-        this.aboutMe = descripción;
-      }
+  constructor(private nombreUsuario: string, private licencia: typeOfLicence, 
+    private edad: number, private descripción: string) {
+      this.username = nombreUsuario;
+      this.age = edad;
+      this.license = licencia;
+      this.aboutMe = descripción;
   }
 
   getUsername() {
     return this.username;
   }
 
-  getEmail() {
-    return this.email;
-  }
-
-  /**
-   * TODO: Añadir un tipo de encriptación a la contraseña
-   */
-  validatePassword(contraseñaPrueba) {
-    if (contraseñaPrueba == this.password) {
-      return true;
-    } else {
-      return false;
-    }
+  getAge() {
+    return this.age;
   }
 
   getLicense() {
