1b844daf34b0be3e83f2e5807a70962c0c79bd7f
Oscar <alu0101038230@ull.edu.es>
2021-12-06 13:31:05 +0000

    Implementada la clase Localizacion

--- src/localizacion.ts
+++ src/localizacion.ts
@@ -1,6 +1,34 @@
+import { Zona } from "./zona";
+
+/**
+ * Interfaz auxiliar para crear un tipado genérico en la clase
+ * sin la necesidad de forzarla a nada más.
+ */
+interface auxiliar<T> {}
+
 /**
  * 
  */
-export class Localizacion {
+export class Localizacion<T> implements auxiliar<T> {
+  private locationName: string;
+  private locationDescription: string;
+  private arrayZone: Zona<T>[] = [];
+  constructor(private nombreLocalizacion: string, private descripcionLocalizacion: string,
+    private arrayZonas: Zona<T>[]) {
+    this.locationName = nombreLocalizacion;
+    this.locationDescription = descripcionLocalizacion;
+    this.arrayZone = arrayZonas;
+  }
 
+  getLocationName() {
+    return this.locationName;
+  }
+
+  getLocationDescription() {
+    return this.locationDescription;
+  }
+
+  getZone(position: number) {
+    return this.arrayZone[position];
+  }
 }
