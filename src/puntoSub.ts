97172dc4968672afe5fd672d93eb4656f8c17fc7
Oscar <alu0101038230@ull.edu.es>
2021-12-06 13:15:58 +0000

    Implementado el buscador de Puntos. Añadidos comentarios a las clases.

--- src/puntoSub.ts
+++ src/puntoSub.ts
@@ -3,21 +3,21 @@
  * Contiene todos los datos de un Punto de Submarinismo.
  * @param pointName Nombre del Punto de Inmersión.
  * @param location Dirección de la localización
- * @param zoneDescription Descripción de la Zona
- * @param dificulty Nivel de dificultad de la zona
+ * @param pointDescription Descripción del Punto de Inmersión
+ * @param dificulty Nivel de dificultad del Punto de Inmersión.
  * @param priceEstimation Array de dos elementos donde se especifica el intervalo de precio de la inmersión.
  */
  export class PuntoSubmarinismo {
   private pointName: string;
   private location: string;
-  private zoneDescription: string;
+  private pointDescription: string;
   private dificulty: string;
   private priceEstimation: number[] = [];
   constructor(private nombrePunto: string, private localizacion: string, private descripcion: string,
     private dificultad: string, private precioEstimacion: number[]) {
     this.pointName = nombrePunto;
     this.location = localizacion;
-    this.zoneDescription = descripcion;
+    this.pointDescription = descripcion;
     this.dificulty =  dificultad;
     this.priceEstimation = precioEstimacion;
   }
@@ -32,21 +32,21 @@
   /**
    * Getter de la localización.
    */
-  getLocation() {
+  getPointLocation() {
     return this.location;
   }
 
   /**
    * Getter de la descripción de la zona.
    */
-  getZoneDescription() {
-    return this.zoneDescription;
+  getPointDescription() {
+    return this.pointDescription;
   }
 
   /**
    * Getter de la dificultad de la zona.
    */
-  getZoneDificulty() {
+  getPointDificulty() {
     return this.dificulty;
   }
 
@@ -54,8 +54,16 @@
    * Getter de la estimación de precio.
    * @return Devuelve el intervalo del precio en formato string.
    */
-  getPriceEstimation() {
+  getPriceEstimationString() {
     let estimation: string = '[' + this.priceEstimation[0] + ' ~ ' + this.priceEstimation[1] + ']'; 
     return estimation;
+  }
+
+  /**
+   * Getter de la estimación de precio.
+   * @return Devuelve el intervalo del precio en formato Array de dos valores enteros.
+   */
+   getPriceEstimationArray() {
+    return this.priceEstimation;
   }
 }
--- src/zona.ts
+++ src/zona.ts
@@ -1,8 +1,18 @@
 import { PuntoSubmarinismo } from "./puntoSub";
+
 /**
- * Clase Zona
+ * Interfaz genérica para poder tener un método de busca
+ * el cual recibe una variable de tipo genérico que se
+ * especifica en función del tipo a buscar.
  */
- export class Zona {
+interface searchEngine<T> {
+  finderOfPoints(searchVariable: T) : PuntoSubmarinismo[];
+}
+
+/**
+ * Clase Zona. Recibe una implementación de la interfaz genérica searchEngine.
+ */
+ export class Zona<T> implements searchEngine<T> {
    /**
     * Constructor de la clase Zona. Contiene todos los datos públicos de las zonas y
     * sus puntos de inmersión.
@@ -52,13 +62,95 @@ import { PuntoSubmarinismo } from "./puntoSub";
   }
 
   /**
-   * TODO: Complete finder of Points (Óscar Pozo)
+   * Busca el string recibido en los Nombres de los Puntos de Inmersión.
+   * Devuelve todos los objetos que lo contienen.
    */
-  finderOfPoints(matchString: string) {
-    this.pointsArray.forEach(element => {
-      if (element.getPointName() === matchString) {
-
+  searchByPointName(searchVariable: string) {
+    const matchedArray: PuntoSubmarinismo[] = [];
+    this.pointsArray.forEach((element) => {
+      if (element.getPointName().search(searchVariable) > -1) {
+        matchedArray.push(element);
       }
     });
+    return matchedArray;
   }
-}
+
+  /**
+   * Busca el string recibido en la Localización de los Puntos de Inmersión.
+   * Devuelve todos los objetos que lo contienen.
+   */
+  searchByPointLocation(searchVariable: string) {
+    const matchedArray: PuntoSubmarinismo[] = [];
+    this.pointsArray.forEach((element) => {
+      if (element.getPointLocation().search(searchVariable) > -1) {
+        matchedArray.push(element);
+      }
+    });
+    return matchedArray;
+  }
+
+  /**
+   * Busca el string recibido en la Descripción de los Puntos de Inmersión.
+   * Devuelve todos los objetos que lo contienen.
+   */
+  searchByPointDescription(searchVariable: string) {
+    const matchedArray: PuntoSubmarinismo[] = [];
+    this.pointsArray.forEach((element) => {
+      if (element.getPointDescription().search(searchVariable) > -1) {
+        matchedArray.push(element);
+      }
+    });
+    return matchedArray;
+  }
+
+  /**
+   * Busca el string recibido en la Dificultad de los Puntos de Inmersión.
+   * Devuelve todos los objetos que lo contienen.
+   * Posible mejora: crear un tipado de Dificultad.
+   */
+  searchByDificulty(searchVariable: string) {
+    const matchedArray: PuntoSubmarinismo[] = [];
+    this.pointsArray.forEach((element) => {
+      if (element.getPointDificulty().search(searchVariable) > -1) {
+        matchedArray.push(element);
+      }
+    });
+    return matchedArray;
+  }
+
+  /**
+   * Comprueba el valor numérico (precio) recibido en el Intervalo de Precio de los Puntos de Inmersión.
+   * Devuelve todos los objetos que lo contienen.
+   */
+  searchByPrice(searchVariable: number) {
+    const matchedArray: PuntoSubmarinismo[] = [];
+    this.pointsArray.forEach((element) => {
+      if (element.getPriceEstimationArray()[0] <= searchVariable &&
+          searchVariable <= element.getPriceEstimationArray()[1]) {
+        matchedArray.push(element);
+      }
+    });
+    return matchedArray;
+  }
+
+  /**
+   * Buscador genérico que busca en todos los atributos de los objetos PuntoSubmarinismo.
+   * Realiza la búsqueda en todos los atributos y devuelve todos los objetos que tienen alguna
+   * coincidencia.
+   */
+   finderOfPoints(searchVariable: T): PuntoSubmarinismo[] {
+    const matchedResults: PuntoSubmarinismo[] = [];
+    if (typeof searchVariable == 'number') {
+      return this.searchByPrice(searchVariable);
+    } else if (typeof searchVariable == 'string') {
+      matchedResults.push(...this.searchByPointName(searchVariable));
+      matchedResults.push(...this.searchByPointLocation(searchVariable));
+      matchedResults.push(...this.searchByPointDescription(searchVariable));
+      matchedResults.push(...this.searchByDificulty(searchVariable));
+    } else {
+      return matchedResults;
+    }
+    return matchedResults;
+  }
+ }
+
