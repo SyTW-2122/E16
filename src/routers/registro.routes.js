3643c31d198695a5bcec4586777488eb8d061677
Oscar <alu0101038230@ull.edu.es>
2022-01-23 14:45:29 +0000

    Añadido registro

---
+++ src/routers/registro.routes.js
@@ -0,0 +1,103 @@
+/*
+- Router() que nos permite crear rutas. Este lo almacenamos en una constante para utilizarla luego.
+
+- Ruta de tipo POST pasamos como parametro el nombre de la ruta seguido de la función que ejecutara.
+La funcion que se le pasa tiene dos parametros req y res
+
+- Para probar nuestra ruta POST tomará el response res utilizará .json para crear un json y enviarlo de vuelta al usuario con el mensaje
+*/
+
+const router = require('express').Router()
+const User = require('../models/users')
+const Joi = require('@hapi/joi')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// Esquema de registro con las valisdaciones de joi
+
+const schemaRegister = Joi.object({
+    nickname: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+
+
+router.post('/login', async (req, res) => {
+    // Validaciones de login
+    const { error } = schemaLogin.validate(req.body)
+    if(error) return res.status(400).json({error: error.details[0].message})
+    
+    // Validaciond e existencia
+    const user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).json({error: 'Usuario no encontrado'})
+
+    // Validacion de password en la base de datos
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if(!validPassword) return res.status(400).json({error: 'Constraseña invalida'})
+
+   // Creando token
+   const token = jwt.sign({
+    name: user.name,
+    id: user._id
+}, "palabra")
+
+// Colocando el token en el header y el cuerpo de la respuesta
+res.header('auth-token', token).json({
+    error: null,
+    data: { token },
+    message: 'Bienvenido'
+    
+    })
+})
+
+
+router.post('/registro', async (req, res) => {
+
+    // Dentro del método que invoca POST 
+    // Usaremos la propiedad error del objeto que nos entrega schemaRegister.validate()
+    const { error } = schemaRegister.validate(req.body)
+
+    // Si este error existe, aqui se termina la ejecución devolviedonos el error
+    if (error) {
+        return res.status(400).json(
+            { error: error.details[0].message }
+        )
+    }
+    
+    const isEmailExist = await User.findOne({ email: req.body.email });
+    if (isEmailExist) {
+        return res.status(400).json(
+            {error: 'Email ya registrado'}
+        )
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const password = await bcrypt.hash(req.body.password, salt)
+
+
+    const user = new User({
+       
+
+        nickname: req.body.nickname,
+        email: req.body.email,
+        password: password
+    });
+    
+    try {
+        const savedUser = await user.save()
+        res.json({
+            error: null,
+            data: savedUser
+        })
+    } catch (error) {
+        res.status(400).json({error})
+    }
+})
+
+module.exports = router
