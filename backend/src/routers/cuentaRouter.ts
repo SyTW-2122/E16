7a697a53f9aa0b419cd763db6b1923ea6c31b775
MisterDesgraciao <oscarpozofdez@gmail.com>
2022-02-04 22:45:52 +0000

    Intento de mejorar el PATCH de Cuenta usando UpdateOne

--- backend/src/routers/cuentaRouter.ts
+++ backend/src/routers/cuentaRouter.ts
@@ -15,24 +15,44 @@ cuentaRouter.get('/cuenta', async (req, res) => {
   }
 });
 
+
+// Método a mejorar, pues crea una cuenta nueva con los mismo parámetros,
+// elimina la anterior y guarda la cuenta nueva.
 cuentaRouter.patch('/cuenta', async (req, res) => {
   const filterID = req.body._id?{_id: req.body._id}:{};
   const cuentaEncontrada = await cuentaModel.findOne(filterID);
   if (!cuentaEncontrada) return res.status(401).json({error: 'No se encontró la cuenta'});
 
   let newNickname = cuentaEncontrada.nickname;
-  if (req.body.nickname.length > 0) {
+  if (typeof req.body.nickname != undefined) {
     newNickname = req.body.nickname;
   }
+
+  let newName = "";
+  if (typeof req.body.name != undefined) {
+    newName = req.body.name;
+  }
+
+  let newSurname = "";
+  if (typeof req.body.surname != undefined) {
+    newSurname = req.body.surname;
+  }
+
+  let newAddress = "";
+  if (typeof req.body.address != undefined) {
+    newAddress = req.body.address;
+  }
+
   const cuentaNueva = new cuentaModel({
     _id: cuentaEncontrada._id,
     nickname: newNickname,
-    name: req.body.name,
-    surname: req.body.surname,
+    name: newName,
+    surname: newSurname,
     email: cuentaEncontrada.email,
     password: cuentaEncontrada.password,
-    address: req.body.address,
+    address: newAddress,
   });
+
   try {
     await cuentaModel.deleteOne({"_id": cuentaEncontrada._id});
     await cuentaNueva.save();
@@ -41,3 +61,43 @@ cuentaRouter.patch('/cuenta', async (req, res) => {
     res.status(400).send(error);
   }
 });
+
+// Intento de actualizar datos usando el updateOne()
+/*
+cuentaRouter.patch('/cuenta', async (req, res) => {
+  const filterID = req.body._id?{_id: req.body._id}:{};
+  const cuentaEncontrada = await cuentaModel.findOne(filterID);
+  if (!cuentaEncontrada) return res.status(401).json({error: 'No se encontró la cuenta'});
+
+  let newvalues = [];
+  if (typeof req.body.nickname != undefined) {
+    const newNickname = {$set: {nickname: req.body.nickname}};
+    newvalues.push(newNickname);
+  }
+
+  if (typeof req.body.name != undefined) {
+    const newName = {$set: {name: req.body.name}};
+    newvalues.push(newName);
+  }
+
+  if (typeof req.body.surname != undefined) {
+    const newSurname = {$set: {name: req.body.surname}};
+    newvalues.push(newSurname);
+  }
+
+  if (typeof req.body.address != undefined) {
+    const newAddress = {$set: {name: req.body.address}};
+    newvalues.push(newAddress);
+  }
+
+  if (newvalues.length > 0) {
+    try {
+      cuentaModel.updateOne(filterID, newvalues, function(err, res) {});
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  } else {
+    res.status(400).send('No se han aportado nuevos datos');
+  }
+});
+*/
